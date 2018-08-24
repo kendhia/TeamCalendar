24325d10414aafe369a90fc783be4fd28acc82ec
Dhia Kennouche <kendhia@gmail.com>
2018-08-24 11:55:57 +0100

    Initial commit

---
+++ app/src/main/java/kendhia/me/projects/teamcalendar/MainActivity.kt
@@ -0,0 +1,12 @@
+package kendhia.me.projects.teamcalendar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
