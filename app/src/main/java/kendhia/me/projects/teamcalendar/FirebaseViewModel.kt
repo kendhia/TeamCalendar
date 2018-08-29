28258424895ecc33ee82912ed3549d9189a1de1b
Dhia Kennouche <kendhia@gmail.com>
2018-08-29 11:49:17 +0100

    fix adapter duplicate items bugs

--- app/src/main/java/kendhia/me/projects/teamcalendar/FirebaseViewModel.kt
+++ app/src/main/java/kendhia/me/projects/teamcalendar/FirebaseViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
+import android.content.Context
 import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DataSnapshot
@@ -11,14 +12,17 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kendhia.me.projects.teamcalendar.Model.TaskEntry
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+
+
 
 class FirebaseViewModel(val app : Application) : AndroidViewModel(app) {
     val ref = FirebaseDatabase.getInstance().reference
-
+    val data = MutableLiveData<List<TaskEntry>>()
 
     fun getTasksByDate( date : String) : LiveData<List<TaskEntry>>{
 
-        val data = MutableLiveData<List<TaskEntry>>()
 
         ref.orderByChild("date").equalTo(date).addValueEventListener( object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
@@ -29,7 +33,10 @@ class FirebaseViewModel(val app : Application) : AndroidViewModel(app) {
                 Log.e("FirebaseViewModel", "date is : $date and its size is ${dataSnapshot.childrenCount}")
                 val items = ArrayList<TaskEntry>()
                 dataSnapshot.children.forEach {
-                    items.add(it.getValue(TaskEntry::class.java)!!)
+                    val t = it.getValue(TaskEntry::class.java)
+                    if (!checkTaskExistBefore(t)) {
+                        items.add(it.getValue(TaskEntry::class.java)!!)
+                    }
                 }
                 data.value = items
             }
@@ -38,6 +45,13 @@ class FirebaseViewModel(val app : Application) : AndroidViewModel(app) {
         return data
     }
 
+    private fun checkTaskExistBefore(taskEntry: TaskEntry?) : Boolean {
+        if (data.value == null) return false
+        data.value!!.forEach {
+            if (it.id == taskEntry!!.id) return true
+        }
+        return false
+    }
     fun getTaskByKey(key : String) : LiveData<TaskEntry> {
 
         val dataItem = MutableLiveData<TaskEntry>()
